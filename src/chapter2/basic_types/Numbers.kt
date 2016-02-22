6fac36e9859444ead788de3b0c785d5319293641
snake <eyesofsnake@live.com>
2016-02-22 23:40:31 +0800

    Numbers #2

--- src/chapter2/basic_types/Numbers.kt
+++ src/chapter2/basic_types/Numbers.kt
@@ -8,17 +8,37 @@ fun main(args: Array<String>) {
     // there is a cached range of Integer, from -128 to 127.
     // when a is in the range, the === comparing is true, false else
     val a: Int = 1000
-    print(a === a) // Prints 'true'
+    println(a === a) // Prints 'true'
     val boxedA: Int? = a
     val anotherBoxedA: Int? = a
-    print(boxedA === anotherBoxedA) // !!!Prints 'false'!!! like equals() method in Java, this is comparing reference
-    print(boxedA == anotherBoxedA) // !!! Prints "true"!!!, this just compares the value
+    println(boxedA === anotherBoxedA) // !!!Prints 'false'!!! like equals() method in Java, this is comparing reference
+    println(boxedA == anotherBoxedA) // !!! Prints "true"!!!, this just compares the value
     println()
 
+    conversions()
+    println()
+    char()
+}
 
-    val b: Int = 1
-    println(b === b)
-    val boxedB: Int? = b
-    val boxedB2: Int? = b
-    println(boxedB === boxedB2)
+fun conversions() {
+//    smaller types are not subtype of bigger one
+//    Hypothetical code,
+//    val a: Int? = 1 // A boxed Int (java.lang.Integer)
+//    val b: Long? = a // implicit conversion yields a boxed Long (java.lang.Long)
+//    print(a == b) print 'false'
+
+//    actually explicit conversion is needed
+    val a: Int = 1;
+    val b: Long = a.toLong()
+//    println(a == b)   // this doesn't compile at all because of type mismatch
+
+    val x =  3 + 3L // type inferred
+    println(x is Long)
+}
+
+fun char() {
+    val str = "hello kotlin"
+    for (c in str) {
+        println("$c --> ${c.toInt()}") // Explicit conversion
+    }
 }

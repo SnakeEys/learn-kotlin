10cc5c3c3a15ea82b4e387e680385ce6b010052d
snake <eyesofsnake@live.com>
2016-02-29 22:19:28 +0800

    finished functions lesson #3

---
+++ src/chapter4/inline_functions.kt
@@ -0,0 +1,17 @@
+package chapter4
+
+import java.util.concurrent.locks.Lock
+
+/**
+ * Created by snake
+ * on 16/2/29.
+ */
+
+fun main(args: Array<String>) {
+
+}
+
+/**
+ * I don't know about inline very much, so just make this empty.
+ * More or less, if you want to get into this, pls check the function lock().
+ */
